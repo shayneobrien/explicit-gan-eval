d9c734b3ad391700f8f837809a1f46b7f5ef4f9b
Septimus Hodge <matthewgroh@gmail.com>
2018-04-26 14:56:07 -0400

    Adding utils file where we compute the PDF, which we can change to MGF

--- gans/utils.py
+++ gans/utils.py
@@ -1,5 +1,7 @@
 import torch
 from torch.autograd import Variable
+import pandas as pd
+import numpy as np
 
 
 def to_var(x):
@@ -7,3 +9,12 @@ def to_var(x):
     if torch.cuda.is_available():
         x = x.cuda()
     return Variable(x)
+
+
+def get_pdf(data):
+    x = []
+    for i in range(data.shape[0]):
+        x.append(list(np.histogram(data[i], bins=100, density=True)[0]))
+    df = pd.DataFrame(x)
+    pdf = list(df.mean(axis=0))
+    return pdf
