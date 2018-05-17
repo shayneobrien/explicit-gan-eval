699c963447f37e1d351c9390419fcb28b1027d89
Septimus Hodge <matthewgroh@gmail.com>
2018-05-17 16:59:56 -0400

    a

--- gan_in_a_box.py
+++ gan_in_a_box.py
@@ -225,7 +225,7 @@ if __name__ == "__main__":
     gans_index = ["wgpgan", "vae"]
     distance_metrics = ["KL-Divergence", "Jensen-Shannon", "Wasserstein-Distance", "Energy-Distance"]
     if data_type == "multivariate":
-        for hyperparam in list(itertools.product(*[learning_rates, hidden_dims, D_steps, BATCH_SIZE])):
+        for hyperparam in list(itertools.product(*[learning_rates, hidden_dims, BATCH_SIZE])):
             lr, dim, step, bsize = hyperparam
             print(hyperparam)
             res = get_multivariate_results(gans, gans_index, distributions, dimensions, epochs, samples, hyperparam)
