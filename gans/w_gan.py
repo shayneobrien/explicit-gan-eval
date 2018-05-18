168120ca4fbc89ca2ea6c0ad6fd89fe3c29f852a
Septimus Hodge <matthewgroh@gmail.com>
2018-05-18 18:02:49 -0400

    a

--- gans/w_gan.py
+++ gans/w_gan.py
@@ -183,10 +183,10 @@ class Trainer:
                 self.wd.append(wd)
                 self.js.append(js)
                 self.ed.append(ed)
-                # print(G_loss[0].data.numpy())
-                # print(D_loss[0].data.numpy())
-                self.gloss.append(G_loss[0].data.cpu().numpy())
-                self.dloss.append(D_loss[0].data.cpu().numpy())
+                print(G_loss[0].data.cpu().numpy())
+                print(D_loss[0].data.cpu().numpy())
+                self.gloss.append(G_loss[0].data.cpu().numpy()[0])
+                self.dloss.append(D_loss[0].data.cpu().numpy()[0])
             # Progress logging
             print ("Epoch[%d/%d], G Loss: %.4f, D Loss: %.4f"
                    %(epoch, num_epochs, np.mean(G_losses), np.mean(D_losses))) 
