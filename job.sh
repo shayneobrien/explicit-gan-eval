e627511542685e567df545697922724ce514c33d
Shayne O'Brien <shayneob@mit.edu>
2018-09-25 13:38:40 -0400

    update

--- job.sh
+++ job.sh
@@ -12,7 +12,7 @@ mixtures=$8
 
 for (( i=job_i; i<=job_j; i++ ))
 do
-  tmux new -d -s $data_type-$samples-$dims-$i #  TODO fix
+  tmux new -d -s $data_type-$samples-$dims-$i
   tmux send -t $data_type-$samples-$dims-$i.0 "CUDA_VISIBLE_DEVICES=$device python3 main.py $data_type $trials $dims 25 $samples $mixtures" ENTER
   sleep 1.05
 done
