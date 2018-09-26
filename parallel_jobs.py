47ad384891c825ef2a53428a6e9f14020ec77ea1
Shayne O'Brien <shayneob@mit.edu>
2018-09-26 00:50:32 -0400

    ok

--- parallel_jobs.py
+++ parallel_jobs.py
@@ -19,8 +19,8 @@ batch_sizes = [
 
 learning_rates = [
                   '2e-1',
-                  '2e-2',
-                  '2e-3',
+                  # '2e-2',
+                  # '2e-3',
                   ]
 
 data_type = 'multivariate'
@@ -29,19 +29,19 @@ dimensions = '32'
 epochs = '25'
 samples = '100000'
 
-device = 0
-job_per_gpu = 8
+device = 4
+job_per_gpu = 7
 
 start_time = datetime.datetime.now().strftime("%Y-%m-%d-%s")
 
 for hdim in hidden_dims:
     for bsize in batch_sizes:
         for lr in learning_rates:
-            device += 1
-            call(['tmux', 'new', '-d', '-s', '{0}-{1}-samples-{2}-dims-{3}-{4}-{5}-{6}'\
+            device += (1/job_per_gpu)
+            call(['echo', 'tmux', 'new', '-d', '-s', '{0}-{1}-samples-{2}-dims-{3}-{4}-{5}-{6}'\
                     .format(data_type, samples, dimensions, trials, lr, bsize, hdim)])
-            call(['tmux', 'send', '-t',
-                  "CUDA_VISIBLE_DEVICES={0}".format(int(floor(device/job_per_gpu))),
+            call(['echo', 'tmux', 'send', '-t',
+                  "CUDA_VISIBLE_DEVICES={0}".format(int(floor(device))),
                   "python3", "mini_main.py",
                   data_type, trials, dimensions, hdim,
                   epochs, samples, bsize, lr, start_time])
