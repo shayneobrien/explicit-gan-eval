bf429dc209f670f219a3efac537c3cb2967e47dd
Shayne O'Brien <shayneob@mit.edu>
2018-09-26 11:19:43 -0400

    make it easier to track progress

--- parallel_jobs.py
+++ parallel_jobs.py
@@ -30,19 +30,31 @@ epochs = '25'
 samples = '100000'
 
 device = 0
-job_per_gpu = 8
+jobs_per_gpu = 8
 
 start_time = datetime.datetime.now().strftime("%Y-%m-%d-%s")
 
 for hdim in hidden_dims:
     for bsize in batch_sizes:
         for lr in learning_rates:
-            device += (1/job_per_gpu)
-            tmux_name = '{0}-{1}-samples-{2}-dims-{3}-{4}-{5}-{6}'.format(data_type, samples, dimensions, trials, lr, bsize, hdim)
-            call(['tmux', 'new', '-d', '-s', tmux_name])
-            call(['tmux', 'send', '-t', tmux_name+'.0',
+            # Only allow certain number of jobs per GPU
+            device += (1/jobs_per_gpu)
+
+            # TMUX session name
+            tmux_name = 'GPU.{7}-{0}-{1}-samples-{2}-dims-{3}-{4}-{5}-{6}'.format(data_type, samples, dimensions,
+                                                                          trials, lr, bsize, hdim, int(floor(device)))
+            # Launch TMUX session
+            call(['echo', 'tmux', 'new', '-d', '-s', tmux_name])
+
+            # Sent the job to that session
+            call(['echo', 'tmux', 'send', '-t', tmux_name+'.0',
                   "CUDA_VISIBLE_DEVICES={0}".format(int(floor(device))),
                   "python3 ", "mini_main.py ",
                   data_type, ' ', trials, ' ', dimensions, ' ', hdim,
                   ' ', epochs, ' ', samples, ' ', bsize, ' ', lr, ' ', start_time,
                   'ENTER'])
+
+            # Send another command to kill the tmux session once it's done running
+            # (easier to track progress using 'tmux ls')
+            call(['echo', 'tmux', 'send', '-t', tmux_name+'.0',
+                  'kill-session ', '-t ', tmux_name, 'ENTER'])
